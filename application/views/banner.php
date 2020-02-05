050bd525b71757a2e328123c462043c1710a2cb0
80010-92-01 <80010-92-01@exchange.ad.afpanet>
2020-02-05 09:47:40 +0100

    banner

---
+++ application/views/banner.php
@@ -0,0 +1,8 @@
+<div class="container-fluid banner">
+    <div class="row">
+        <div class="col">
+            <a href="http://www.corif.fr/">
+            <img src="<?= base_url("assets/img/images/logo-b.jpg") ?>" alt="CORIF" class="mx-auto d-block img-fluid"></a>
+        </div>
+    </div>
+</div> 
