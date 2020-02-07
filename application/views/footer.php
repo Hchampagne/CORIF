af6f0a357db60901a644df06d7b316801c4b780a
Hchampagne <51380093+Hchampagne@users.noreply.github.com>
2020-02-07 07:19:55 +0100

    alignement footer

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,26 +1,26 @@
 
 <footer class="page-footer font-small">  
   <div class="container-fluid ">
-    <div class="row ">  
-        <div class="col-sm-4">       
+    <div class="row justify-content-around">  
+        <div class="col-sm-2">       
             <ul>
-                <li><h5 class="text-uppercase font-weight-bold" id="footer">Adresse</h5></li>
+                <li class="text-uppercase font-weight-bold" id="footer">Adresse</li>
                 <li>CORIF</li>
                 <li>145 rues des stations</li>
                 <li>59000 LILLE</li>
             </ul>
       </div>
-      <div class="col-sm-4">
+      <div class="col-sm-2">
             <ul>              
-                <li><h5 class="text-uppercase font-weight-bold" id="footer">CONTACTS</h5></li>
+                <li class="text-uppercase font-weight-bold" id="footer">CONTACT</li>
                 <li>&nbsp</li>
                 <li>tel: 03 20 54 73 55</li>
                 <li>Mail: <a href="">contact@corif.fr</a></li>
             </ul>
       </div>
-      <div class="col-sm-4">
+      <div class="col-sm-2">
             <ul>              
-                <li><h5 class="text-uppercase font-weight-bold" id="footer">mentions légales</h5></li>
+                <li class="text-uppercase font-weight-bold" id="footer">mentions légales</li>
                 <li>&nbsp</li>
                 <li><a href="#"> Mentions légales</a></li>
                 <li>CORIF 2015</li>
