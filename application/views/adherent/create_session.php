29bb9ba72f5739cfa44aaa048b73abd0c423a06f
Hchampagne <51380093+Hchampagne@users.noreply.github.com>
2020-02-10 22:01:35 +0100

    form /session

--- application/views/adherent/create_session.php
+++ application/views/adherent/create_session.php
@@ -7,7 +7,24 @@
 
     <form action="" method="post">
 
+        <div class="row">
+            <div class="col">
+                <label for="ses_date">Date : </label>
+                <input type="date" name="ses_date" id="ses_time">
+            </div>
+            <div class="col">
+                <label for="ses_h_debut">H de début : </label>
+                <input type="time" name="ses_h_debut" id="ses_time">
+            </div>
+            <div class="col">
+                <label for="ses_h_fin">H de fin : </label>
+                <input type="time" name="ses_h_fin" id="ses_time">
+            </div>
+            <div class="col-6">
+                
+            </div>
 
-
+        </div>
     </form>
-</div>
+
+</div><!--container -->
