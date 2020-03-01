64fdfad884ddf3498dc68986bad5e45a60a734ce
Hchampagne <51380093+Hchampagne@users.noreply.github.com>
2020-03-01 22:13:33 +0100

    drop zone

--- application/views/espace_jeu/espace_invite.php
+++ application/views/espace_jeu/espace_invite.php
@@ -16,10 +16,10 @@
 
    <div class="row">
       <div class="col">
-         <div class="row">
-         
+         <div class="row" >
+
             <?php foreach ($cartes as $carte) { ?>
-               <div class="card"  id="<?= $carte->car_id; ?>" class="ui-widget-content" style="width: 18rem; height: 24ren;">
+               <div   id="<?= $carte->car_id; ?>" class="card ui-widget-content" style="width: 14rem;">
                   <img src="<?= base_url("assets/img/images/logo-b.jpg") ?>" class="card-img-top">
                   <div class="card-body">
                      <h5 class="card-title"><?= $carte->car_numero ?></h5>
@@ -37,18 +37,24 @@
 </div>
 <script>
    $(function() {
-
-      $(".card").click(function() {
-         $(this).draggable({cursor:  "crosshair",cursorAt: {top: -5, left:-5}});
-      });
+      
+     
+      $(".card").draggable({cursor:  "blank"});
+         
 
       $( "#target" ).droppable({
       drop: function( event, ui ) {
-        $( this )
-          .addClass( "ui-state-highlight" )
-          .find( "p" )
-            .html( "Ajout:" );
-      }
+         
+                $(this).css('background', 'rgb(0,200,0)');
+            },
+            over: function(event, ui) {
+                $(this).css('background', 'orange');
+            },
+            out: function(event, ui) {
+                $(this).css('background', 'cyan');
+            }
+          
+     
     });
 
 
