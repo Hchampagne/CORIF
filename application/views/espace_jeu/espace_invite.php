c5af91aa7bc61b8ad96919f11a217bedf29eb041
80010-92-01 <80010-92-01@exchange.ad.afpanet>
2020-03-02 17:08:15 +0100

    jeu /dragdrop

--- application/views/espace_jeu/espace_invite.php
+++ application/views/espace_jeu/espace_invite.php
@@ -1,64 +1,134 @@
-
-
-
 <div class="container-fluid">
    <div class="row">
       <div class="col">
          <div class="row">
-            <div id="target" class="col-3 ui-widget-header">
-               <p>drop zone</p>
+            <div class="col">
+               <hr>
+               <button id="ajoutPile" class="btn">Ajout pile</button>
+               <hr>
             </div>
          </div>
       </div>
    </div>
 
 
+   <div class="row">
+      <div class="col">
+         <div class="aireJeu row justify-content-center">
+            <div class="pile col-3 ">
+               <div class="target ui-widget-header" id="target0">
+                  <p>drop zone</p>
+               </div>
+               <input type="text" name="reponseMetier" id="reponseMetier" placeholder="Réponse">
+            </div>
+         </div>
+      </div>
+   </div>
 
    <div class="row">
       <div class="col">
-         <div class="row" >
+         <div class="row">
+            <div class="col">
+               <hr>
+               <p id="compteur"> &nbsp</p>
+               <hr>
+            </div>
+         </div>
+      </div>
+   </div>
 
-            <?php foreach ($cartes as $carte) { ?>
-               <div   id="<?= $carte->car_id; ?>" class="card ui-widget-content" style="width: 14rem;">
-                  <img src="<?= base_url("assets/img/images/logo-b.jpg") ?>" class="card-img-top">
-                  <div class="card-body">
-                     <h5 class="card-title"><?= $carte->car_numero ?></h5>
-                     <p><?= $carte->car_type ?></p>
-                     <p class="card-text"><?= $carte->car_description ?></p>
-                  </div>
+   <div class="row">
+      <div class="col">
+         <div class="row justify-content-center">
+            <div class="col-3">
+               <div id="start" class="start ui-widget-header">
+                  <?php foreach ($cartes as $carte) { ?>
+                     <div id="<?= $carte->car_id; ?>" z-index="<?= $carte->car_id; ?>" class="card position-absolute ui-widget-content">
+                        <img src="<?= base_url("assets/img/images/logo-b.jpg") ?>" class="card-img-top">
+                        <div class="card-body">
+                           <h5 class="card-title"><?= $carte->car_numero ?></h5>
+                           <p><?= $carte->car_type ?></p>
+                           <p class="card-text"><?= $carte->car_description ?></p>
+                        </div>
+                     </div>
+                  <?php } ?>
                </div>
-               <?php } ?>
-            
-            
+            </div>
          </div>
       </div>
    </div>
 
 </div>
+
 <script>
-   $(function() {
-      
-     
-      $(".card").draggable({cursor:  "blank"});
-         
+   $(document).ready(function() {
 
-      $( "#target" ).droppable({
-      drop: function( event, ui ) {
-         
-                $(this).css('background', 'rgb(0,200,0)');
+      // Zone de drop
+      var ZD = "<div class=\"pile col-3 \">" +
+         "<div class=\"target ui-widget-header \" id=\"traget1\">" +
+         "<p>drop zone</p>" +
+         "</div>" +
+         "<input type=\"text\" name=\"reponseMetier\" id=\"reponseMetier\" placeholder=\"Réponse\">" +
+         "</div>";
+
+      // initialise le compteur carte
+      var compte = 0;
+
+      // ajout drop zone
+      $("#ajoutPile").on("click", function() {
+         $(ZD).appendTo(".aireJeu");
+
+         $(".target").droppable({
+
+            drop: function(event, ui) {
+               $(this).css('background', 'rgb(0,200,0)');
             },
+
             over: function(event, ui) {
-                $(this).css('background', 'orange');
+               $(this).css('background', 'orange');
             },
+
             out: function(event, ui) {
-                $(this).css('background', 'cyan');
+               $(this).css('background', 'rgb(0,200,0)');
+               
             }
-          
-     
-    });
+         });
+      })
 
+      // rend les cartes draggable
+      $(".card").draggable();
 
+      // zone de départ tas initial droppable + compteur deplacement
+      $(".start").droppable({
 
+         drop: function(event, ui) {
+            $(this).css('background', 'rgb(0,200,0)');
+            compte += 1;
+            $("#compteur").text("compteur: " + compte);
+         },
+         over: function(event, ui) {
+            $(this).css('background', 'orange');
+         },
+         out: function(event, ui) {
+            $(this).css('background', 'rgb(0,200,0)');
+            compte -= 1;
+            $("#compteur").text("compteur: " + compte);
+         }
+      });
+
+      // zone de dépot initiale droppable 
+      $(".target").droppable({
+
+         drop: function(event, ui) {
+            $(this).css('background', 'rgb(0,200,0)');           
+         },
+         over: function(event, ui) {
+            $(this).css('background', 'orange');
+         },
+         out: function(event, ui) {
+            $(this).css('background', 'rgb(0,200,0)');
+         }
+      });
 
    });
 </script>
